7ec362c02fbb576319fe9da7f66179c5f6a23dd2
tiagovrs <tiago.vsant@gmail.com>
2025-06-17 21:43:43 -0300

    1.2: Course Information, step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,13 +6,22 @@ const Header = (props) => {
   )
 }
 
+// Go Horse Approach 
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} - {props.exercise}
+    </p>
   )
 }
 
@@ -38,9 +47,7 @@ const App = () => {
   return (
     <div>
       <Header course={ course } />
-      <Content part={ part1 } exercises={ exercises1 } />
-      <Content part={ part2 } exercises={ exercises2 } />
-      <Content part={ part3 } exercises={ exercises3 } />
+      <Content part1 = {part1} part2={part2} part3={part3} exercise1 = {exercises1} exercise2 = {exercises2} exercise3 = {exercises3} />
       <Total exercises = { exercises1 + exercises2 + exercises3} />
     </div>
   )
